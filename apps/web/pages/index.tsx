ea68615dc7f682616c5c98dce3c253cb89355848
ohshyuk5 <seunghyukoh0@gmail.com>
2022-11-08 21:06:12 +0900

    Query at server side

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,4 +1,6 @@
-import { Button } from "ui";
+import { Button } from 'ui';
+import { allPostsQueryVars, ALL_POSTS_QUERY } from '../components/PostList';
+import { addApolloState, initializeApollo } from '../lib/apolloClient';
 
 export default function Web() {
   return (
@@ -7,4 +9,18 @@ export default function Web() {
       <Button />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: ALL_POSTS_QUERY,
+    variables: allPostsQueryVars,
+  });
+
+  return addApolloState(apolloClient, {
+    props: {},
+    revalidate: 1,
+  });
 }
