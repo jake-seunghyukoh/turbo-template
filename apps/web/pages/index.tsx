93242f5680f861580cd8ffbdcc1e3ec9b6421066
ohshyuk5 <seunghyukoh0@gmail.com>
2022-11-08 22:23:04 +0900

    Add link component

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,13 @@
-import { Button } from "ui";
+import Button from '@mui/material/Button';
+import { Link } from '../src/components/atoms/link';
 
-export default function Web() {
+export default function Home() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>Home</h1>
+      <Button href="/foo" component={Link}>
+        To foo
+      </Button>
     </div>
   );
 }
