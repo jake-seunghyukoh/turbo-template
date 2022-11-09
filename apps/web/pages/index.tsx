4ec6d6d783c346262aeb7f5febe0b207ddbbac0d
ohshyuk5 <seunghyukoh0@gmail.com>
2022-11-09 17:43:57 +0900

    Set up next auth

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,9 @@
 import HomePage from '@page-components/home';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <div>
       <HomePage />
