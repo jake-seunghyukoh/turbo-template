d6f8cd3c4b4942681b6839fdd0a84e8fb2f4ccfc
ohshyuk5 <seunghyukoh0@gmail.com>
2022-11-10 21:20:43 +0900

    Fix sign in redirection issue

--- apps/web/pages/sign-in.tsx
+++ apps/web/pages/sign-in.tsx
@@ -1,8 +1,32 @@
 import SignInPage from '@page-components/sign-in';
+import { GetServerSidePropsContext } from 'next';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
+
 export default function SignIn() {
   return (
     <div>
       <SignInPage />
     </div>
   );
+}
+
+export async function getServerSideProps({
+  req,
+  res,
+}: GetServerSidePropsContext) {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
